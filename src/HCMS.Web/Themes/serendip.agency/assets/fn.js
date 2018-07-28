22c7b87e61be928ffdaa8a2c9c91be2edab12def
Mohsen Esmaeli <m-esm@hotmail.com>
2018-07-29 02:42:58 +0430

    init happy & serendip themes

---
+++ src/HCMS.Web/Themes/serendip.agency/assets/fn.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 700);
+    });
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 200)
+            $('#backToTop').removeClass('clicked');
+        else
+            $('#backToTop').addClass('clicked');
+
+
+    });
+
+    $('#backToTop').click(function () {
+
+        var btn = $(this);
+
+        btn.addClass('clicked');
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+
+    });
+
+
+});
